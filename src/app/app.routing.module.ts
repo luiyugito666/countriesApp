c06d9a3a108d55dafb70f5df870eaeea3210654b
yuguito <yugito_is@hotmail.com>
2023-06-13 14:00:38 -0500

    creaccion de paginas y routing

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+const routes: Routes = [
+  {
+    path: '',
+    component:HomePageComponent
+
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+
+  },
+  {
+    path: '**',
+    redirectTo: ''
+
+  }
+
+]
+
+@NgModule({
+
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class AppRoutingModule { }
